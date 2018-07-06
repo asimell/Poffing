7e247c6eaf0d96f11f1253ef26400391e678c77a
Aleksi Simell <simell.aleksi@gmail.com>
2018-07-06 12:33:04 +0300

    Enable camera zoom

--- Poffing/Assets/Scripts/CameraController.cs
+++ Poffing/Assets/Scripts/CameraController.cs
@@ -9,14 +9,19 @@ public class CameraController : MonoBehaviour {
     private Camera cam;
     private readonly float xMin = 10f;
     private readonly float xMax = 55f;
+    private readonly float cameraMin = 3f;
+    private readonly float cameraMax = 8f;
+    private float originalSize;
     private Vector3 originalPos;
     private Quaternion originalRot;
+    
 
     // Use this for initialization
     void Start () {
         cam = GetComponentInChildren<Camera>();
         originalPos = cam.transform.position;
         originalRot = cam.transform.rotation;
+        originalSize = cam.orthographicSize;
 	}
 	
 	// Update is called once per frame
@@ -26,6 +31,15 @@ public class CameraController : MonoBehaviour {
             ResetCameraRotation();
             return;
         }
+
+        if (Input.GetKey(KeyCode.Comma))
+        {
+            ZoomIn();
+        } else if (Input.GetKey(KeyCode.Period))
+        {
+            ZoomOut();
+        }
+
 
         float x = Input.GetAxis("CameraVertical");
         float y = Input.GetAxis("CameraHorizontal");
@@ -41,9 +55,28 @@ public class CameraController : MonoBehaviour {
         transform.RotateAround(Vector3.zero, movementY, 20f * Time.deltaTime * speed);
     }
 
+    private void ZoomIn()
+    {
+        if (cam.orthographicSize > cameraMin)
+        {
+            cam.orthographicSize -= 0.05f;
+
+        }
+    }
+
+    private void ZoomOut()
+    {
+        if (cam.orthographicSize < cameraMax)
+        {
+            cam.orthographicSize += 0.05f;
+
+        }
+    }
+
     private void ResetCameraRotation()
     {
         cam.transform.position = originalPos;
         cam.transform.rotation = originalRot;
+        cam.orthographicSize = originalSize;
     }
 }
